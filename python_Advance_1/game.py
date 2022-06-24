74d2f8dc2ca5030691ae61ca7a628c7a02073725
frey <free3y@gmail.com>
2022-06-24 23:49:13 +0530

    game

---
+++ python_Advance_1/game.py
@@ -0,0 +1,12 @@
+#OOP
+
+class Playercharacter:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        print('run')
+
+    player1 = Playercharacter('Frey')
+
+    print(player1)
